5fcafb3f8a53afbd71f1e4bf8993ea602e3a1e78
Prashant Bansal <prashantbansal@Prashants-MacBook-Pro.local>
2023-12-13 23:43:28 +0530

    Updates

---
+++ precache-manifest.35f7c1586abb8f5a0586573c3ea1b0fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3ed908c95035913acca1f979d69e3f9",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "dffc04c0826e2b01937b",
+    "url": "/resume/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "415e46f1a2a3956610e0",
+    "url": "/resume/static/js/2.e4a3e389.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/resume/static/js/2.e4a3e389.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dffc04c0826e2b01937b",
+    "url": "/resume/static/js/main.def8bbad.chunk.js"
+  },
+  {
+    "revision": "2b17229f8e36d952a251",
+    "url": "/resume/static/js/runtime-main.6275bf14.js"
+  }
+]);
